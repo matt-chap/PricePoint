971caf3d1a01d4e900a358d2cddc84059fed35d5
Matt Chapman <eatsalldaylol@hotmail.com>
2021-08-23 22:03:07 -0400

    Backgrounds for each page

--- template_app/src/pages/public/aboutPage.js
+++ template_app/src/pages/public/aboutPage.js
@@ -2,20 +2,8 @@ import React from 'react'
 
 export const AboutPage = () => {
     return (
-        <div className="App">
-            <header className="App-header">
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+        <div className="about-background">
+            
         </div>
     )
 }
