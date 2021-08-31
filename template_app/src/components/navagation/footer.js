6a5903d15d1c64709c2b54866a34444e8ed34621
Matt Chapman <eatsalldaylol@hotmail.com>
2021-08-30 22:01:03 -0400

    Footer Template

--- template_app/src/components/navagation/footer.js
+++ template_app/src/components/navagation/footer.js
@@ -1,25 +1,37 @@
 import React from 'react'
-import { Row, Col } from 'react-bootstrap'
+import { Row, Col, Container } from 'react-bootstrap'
 import styles from './footer.module.scss'
 
 export const Footer = () => {
     return (
         <div className={`${styles.footer_background} bg-dark`}>
-            <Row>
-                <Col>1 of 2</Col>
-                <Col>2 of 2</Col>
-                <Col>2 of 2</Col>
-            </Row>
-            <Row>
-                <Col>1 of 3</Col>
-                <Col>2 of 3</Col>
-                <Col>3 of 3</Col>
-            </Row>
-            <Row>
-                <Col>1 of 3</Col>
-                <Col>2 of 3</Col>
-                <Col>3 of 3</Col>
-            </Row>
+            <Container>
+            <Row md={12}>
+                <Col md={8}>
+                    <Row md={12} className={styles.bold_text}>
+                        <Col md={4}>About</Col>
+                        <Col md={4}>Help Center</Col>
+                    </Row>
+                    <Row md={12}>
+                        <Col md={4}>Terms and Conditions</Col>
+                        <Col md={4}>Contact Us</Col>
+                    </Row>
+                    <Row md={12}>
+                        <Col md={4}>Privacy</Col>
+                        <Col md={4}>FAQ</Col>
+                    </Row>
+                </Col>
+                <Col md={4} style={{}} className={styles.center_items}>
+                    <Row md={12}>
+                        <Col md={12}>APPs</Col>
+                    </Row>
+                    <Row md={12}>
+                        <Col md={6}>iOS</Col>
+                        <Col md={6}>Android</Col>
+                    </Row>
+                </Col>
+                </Row>
+            </Container>
         </div>
     )
 }
