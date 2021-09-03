a096d00c4c6982f1f79bc8aefbcbff04161a3db8
Matt Chapman <eatsalldaylol@hotmail.com>
2021-09-02 22:23:36 -0400

    Add landing page main div

--- template_app/src/pages/public/landingPage.js
+++ template_app/src/pages/public/landingPage.js
@@ -1,18 +1,16 @@
 import React from 'react'
+import { Row, Col, Container } from 'react-bootstrap'
 import styles from './landingPage.module.scss'
 
 export const LandingPage = () => {
     return (
         <div className={styles.landing_background} style={{height:"100vh"}}>
-            <div>
-
-            </div>
-            <div>
-                
-            </div>
-            <div>
-                
-            </div>
+            <Container>
+            <Row md={12} className={styles.image_container}>
+                <Col md={12}>
+                </Col>
+            </Row>
+            </Container>
         </div>
     )
 }
