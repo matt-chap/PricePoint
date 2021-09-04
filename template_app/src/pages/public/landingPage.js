5b0e15ebc9cdd1176319fe292c980cd9140b6a8f
Matt Chapman <eatsalldaylol@hotmail.com>
2021-09-03 20:27:09 -0400

    Landing page update

--- template_app/src/pages/public/landingPage.js
+++ template_app/src/pages/public/landingPage.js
@@ -4,10 +4,18 @@ import styles from './landingPage.module.scss'
 
 export const LandingPage = () => {
     return (
-        <div className={styles.landing_background} style={{height:"100vh"}}>
-            <Container>
+        <div className={styles.landing_background}>
+            <Container fluid>
             <Row md={12} className={styles.image_container}>
                 <Col md={12}>
+                </Col>
+            </Row>
+            <Row md={12} style={{backgroundColor:"red",height:"180px"}}>
+                <Col md={4}>
+                </Col>
+                <Col md={4}>
+                </Col>
+                <Col md={4}>
                 </Col>
             </Row>
             </Container>
