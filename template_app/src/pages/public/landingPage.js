622f229b33476a0dfd72e6d5a867660cbd4cca92
Matt Chapman <eatsalldaylol@hotmail.com>
2021-09-09 21:22:00 -0400

    Add more landing components

--- template_app/src/pages/public/landingPage.js
+++ template_app/src/pages/public/landingPage.js
@@ -7,27 +7,108 @@ export const LandingPage = () => {
     return (
         <div className={styles.landing_background}>
             <Container fluid>
-            <Row md={12} className={styles.image_container}>
-                <Col md={12}>
-                </Col>
-            </Row>
-            <Row md={12} className={styles.landing_about}>
-                <Col md={4}>
-                    <LocalGroceryStore style={{fontSize: "10rem"}}/>
-                </Col>
-                <Col md={4}>
-                    <Storefront style={{fontSize: "10rem"}}/>
-                </Col>
-                <Col md={4}>
-                    <Devices style={{fontSize: "10rem"}}/>
-                </Col>
-            </Row>
-            <Row md={12} className={styles.landing_about}>
-                <Col md={6}>
-                </Col>
-                <Col md={6}>
-                </Col>
-            </Row>
+                <Row md={12} className={styles.image_container}>
+                    <Col md={12}>
+                    </Col>
+                </Row>
+                <Row md={12} className={styles.landing_about}>
+                    <Col md={4}>
+                        <Row>
+                            <Col md={12}>
+                                <LocalGroceryStore style={{ fontSize: "10rem" }} />
+                            </Col>
+                            <Col md={12} className={styles.summary_text}>
+                            Quisque pharetra tortor sit amet euismod faucibus. 
+                            Integer nibh neque, mattis vitae laoreet non, facilisis et nulla. 
+                            Integer ut mi aliquet, vulputate eros placerat, venenatis urna. 
+                            Vestibulum venenatis quis turpis quis tincidunt. 
+                            Suspendisse placerat, augue ut laoreet iaculis, metus velit ultricies ipsum, nec egestas ex lacus eu dolor. 
+                            Nunc faucibus ultrices dui, ut vehicula arcu euismod vitae. 
+                            Vestibulum viverra massa nec justo eleifend, sit amet sollicitudin turpis finibus. Donec in suscipit odio. Integer consequat. 
+                            </Col>
+                        </Row>
+                    </Col>
+                    <Col md={4}>
+                        <Row>
+                            <Col md={12}>
+                                <Storefront style={{ fontSize: "10rem" }} />
+                            </Col>
+                            <Col md={12} className={styles.summary_text}>
+                            Quisque pharetra tortor sit amet euismod faucibus. 
+                            Integer nibh neque, mattis vitae laoreet non, facilisis et nulla. 
+                            Integer ut mi aliquet, vulputate eros placerat, venenatis urna. 
+                            Vestibulum venenatis quis turpis quis tincidunt. 
+                            Suspendisse placerat, augue ut laoreet iaculis, metus velit ultricies ipsum, nec egestas ex lacus eu dolor. 
+                            Nunc faucibus ultrices dui, ut vehicula arcu euismod vitae. 
+                            Vestibulum viverra massa nec justo eleifend, sit amet sollicitudin turpis finibus. Donec in suscipit odio. Integer consequat. 
+                            </Col>
+                        </Row>
+                    </Col>
+                    <Col md={4}>
+                        <Row>
+                            <Col md={12}>
+                                <Devices style={{ fontSize: "10rem" }} />
+                            </Col>
+                            <Col md={12}  className={styles.summary_text} >
+                            Quisque pharetra tortor sit amet euismod faucibus. 
+                            Integer nibh neque, mattis vitae laoreet non, facilisis et nulla. 
+                            Integer ut mi aliquet, vulputate eros placerat, venenatis urna. Vestibulum venenatis quis turpis quis tincidunt. Suspendisse placerat, augue ut laoreet iaculis, metus velit ultricies ipsum, nec egestas ex lacus eu dolor. Nunc faucibus ultrices dui, ut vehicula arcu euismod vitae. 
+                            Vestibulum viverra massa nec justo eleifend, sit amet sollicitudin turpis finibus. Donec in suscipit odio. Integer consequat. 
+                            </Col>
+                        </Row>
+                    </Col>
+                </Row>
+                <Row md={12} className={styles.image_container}>
+                    <Col md={12}>
+                    </Col>
+                </Row>
+                <Row md={12} className={styles.landing_about}>
+                    <Col md={4}>
+                        <Row>
+                            <Col md={12}>
+                                <LocalGroceryStore style={{ fontSize: "10rem" }} />
+                            </Col>
+                            <Col md={12} className={styles.summary_text}>
+                            Quisque pharetra tortor sit amet euismod faucibus. 
+                            Integer nibh neque, mattis vitae laoreet non, facilisis et nulla. 
+                            Integer ut mi aliquet, vulputate eros placerat, venenatis urna. 
+                            Vestibulum venenatis quis turpis quis tincidunt. 
+                            Suspendisse placerat, augue ut laoreet iaculis, metus velit ultricies ipsum, nec egestas ex lacus eu dolor. 
+                            Nunc faucibus ultrices dui, ut vehicula arcu euismod vitae. 
+                            Vestibulum viverra massa nec justo eleifend, sit amet sollicitudin turpis finibus. Donec in suscipit odio. Integer consequat. 
+                            </Col>
+                        </Row>
+                    </Col>
+                    <Col md={4}>
+                        <Row>
+                            <Col md={12}>
+                                <Storefront style={{ fontSize: "10rem" }} />
+                            </Col>
+                            <Col md={12} className={styles.summary_text}>
+                            Quisque pharetra tortor sit amet euismod faucibus. 
+                            Integer nibh neque, mattis vitae laoreet non, facilisis et nulla. 
+                            Integer ut mi aliquet, vulputate eros placerat, venenatis urna. 
+                            Vestibulum venenatis quis turpis quis tincidunt. 
+                            Suspendisse placerat, augue ut laoreet iaculis, metus velit ultricies ipsum, nec egestas ex lacus eu dolor. 
+                            Nunc faucibus ultrices dui, ut vehicula arcu euismod vitae. 
+                            Vestibulum viverra massa nec justo eleifend, sit amet sollicitudin turpis finibus. Donec in suscipit odio. Integer consequat. 
+                            </Col>
+                        </Row>
+                    </Col>
+                    <Col md={4}>
+                        <Row>
+                            <Col md={12}>
+                                <Devices style={{ fontSize: "10rem" }} />
+                            </Col>
+                            <Col md={12}  className={styles.summary_text} >
+                            Quisque pharetra tortor sit amet euismod faucibus. 
+                            Integer nibh neque, mattis vitae laoreet non, facilisis et nulla. 
+                            Integer ut mi aliquet, vulputate eros placerat, venenatis urna. Vestibulum venenatis quis turpis quis tincidunt. Suspendisse placerat, augue ut laoreet iaculis, metus velit ultricies ipsum, nec egestas ex lacus eu dolor. Nunc faucibus ultrices dui, ut vehicula arcu euismod vitae. 
+                            Vestibulum viverra massa nec justo eleifend, sit amet sollicitudin turpis finibus. Donec in suscipit odio. Integer consequat. 
+                            </Col>
+                        </Row>
+                    </Col>
+                </Row>
             </Container>
         </div>
     )
