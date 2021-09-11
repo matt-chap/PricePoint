3483898e6f19869db46fd4bcd99860c5376efe6e
Matt Chapman <eatsalldaylol@hotmail.com>
2021-09-10 23:28:47 -0400

    Clean up Landing

--- template_app/src/pages/public/landingPage.js
+++ template_app/src/pages/public/landingPage.js
@@ -66,45 +66,24 @@ export const LandingPage = () => {
                     <Col md={4}>
                         <Row>
                             <Col md={12}>
-                                <LocalGroceryStore style={{ fontSize: "10rem" }} />
                             </Col>
                             <Col md={12} className={styles.summary_text}>
-                            Quisque pharetra tortor sit amet euismod faucibus. 
-                            Integer nibh neque, mattis vitae laoreet non, facilisis et nulla. 
-                            Integer ut mi aliquet, vulputate eros placerat, venenatis urna. 
-                            Vestibulum venenatis quis turpis quis tincidunt. 
-                            Suspendisse placerat, augue ut laoreet iaculis, metus velit ultricies ipsum, nec egestas ex lacus eu dolor. 
-                            Nunc faucibus ultrices dui, ut vehicula arcu euismod vitae. 
-                            Vestibulum viverra massa nec justo eleifend, sit amet sollicitudin turpis finibus. Donec in suscipit odio. Integer consequat. 
                             </Col>
                         </Row>
                     </Col>
                     <Col md={4}>
                         <Row>
                             <Col md={12}>
-                                <Storefront style={{ fontSize: "10rem" }} />
                             </Col>
                             <Col md={12} className={styles.summary_text}>
-                            Quisque pharetra tortor sit amet euismod faucibus. 
-                            Integer nibh neque, mattis vitae laoreet non, facilisis et nulla. 
-                            Integer ut mi aliquet, vulputate eros placerat, venenatis urna. 
-                            Vestibulum venenatis quis turpis quis tincidunt. 
-                            Suspendisse placerat, augue ut laoreet iaculis, metus velit ultricies ipsum, nec egestas ex lacus eu dolor. 
-                            Nunc faucibus ultrices dui, ut vehicula arcu euismod vitae. 
-                            Vestibulum viverra massa nec justo eleifend, sit amet sollicitudin turpis finibus. Donec in suscipit odio. Integer consequat. 
                             </Col>
                         </Row>
                     </Col>
                     <Col md={4}>
                         <Row>
                             <Col md={12}>
-                                <Devices style={{ fontSize: "10rem" }} />
                             </Col>
-                            <Col md={12}  className={styles.summary_text} >
-                            Quisque pharetra tortor sit amet euismod faucibus. 
-                            Integer nibh neque, mattis vitae laoreet non, facilisis et nulla. 
-                            Integer ut mi aliquet, vulputate eros placerat, venenatis urna. Vestibulum venenatis quis turpis quis tincidunt. Suspendisse placerat, augue ut laoreet iaculis, metus velit ultricies ipsum, nec egestas ex lacus eu dolor. Nunc faucibus ultrices dui, ut vehicula arcu euismod vitae. 
-                            Vestibulum viverra massa nec justo eleifend, sit amet sollicitudin turpis finibus. Donec in suscipit odio. Integer consequat. 
+                            <Col md={12} className={styles.summary_text}>
                             </Col>
                         </Row>
                     </Col>
