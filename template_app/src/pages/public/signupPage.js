67d24954462d0b3e5e6d877777419db6098fb33a
Matt Chapman <eatsalldaylol@hotmail.com>
2021-09-14 20:37:12 -0400

    Clean up sign in page

--- template_app/src/pages/public/signupPage.js
+++ template_app/src/pages/public/signupPage.js
@@ -9,41 +9,42 @@ export const SignupPage = () => {
 
     return (
         <Container fluid className={styles.signup_background}>
-            <Row >
-                <Col xs={0} md={2} lg={3}></Col>
-                <Col>
-                    <div className={styles.fullHeight}>
-                        <h2 style={{ marginBottom: '20px' }}>Sign Up</h2>
-                        <Form onSubmit={handleSignUp}>
-                            <Form.Group controlid="firstName">
-                                <Form.Label>First Name</Form.Label>
-                                <Form.Control required type="text" placeholder="First Name" />
-                            </Form.Group>
-                            <Form.Group controlid="lastName">
-                                <Form.Label>Last Name</Form.Label>
-                                <Form.Control required type="text" placeholder="Last Name" />
-                            </Form.Group>
-                            <Form.Group>
-                                <Form.Label>Phone Number</Form.Label>
-                                <Form.Control required type="text" placeholder="Phone Number" />
-                            </Form.Group>
-                            <Form.Group controlId="email">
-                                <Form.Label>Email Address</Form.Label>
-                                <Form.Control required type="email" placeholder="Enter Email" />
-                            </Form.Group>
-                            <Form.Group controlId="password">
-                                <Form.Label>Password</Form.Label>
-                                <Form.Control required type="password" placeholder="Password" />
-                            </Form.Group>
-                            <div>
-                                <Button variant="primary" type="submit">
-                                    Sign Up
-                                </Button>
-                            </div>
-                        </Form>
+            <Row className={styles.row_padding}>
+                <Col xs={0} sm={0} md={1} lg={1}></Col>
+                <Col sm={12} md={6} lg={3} className={styles.row_padding}>
+                    <Form onSubmit={handleSignUp}>
+                        <Form.Group controlid="firstName">
+                            <Form.Label>First Name</Form.Label>
+                            <Form.Control required type="text" placeholder="First Name" />
+                        </Form.Group>
+                        <Form.Group controlid="lastName">
+                            <Form.Label>Last Name</Form.Label>
+                            <Form.Control required type="text" placeholder="Last Name" />
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Label>Phone Number</Form.Label>
+                            <Form.Control required type="text" placeholder="Phone Number" />
+                        </Form.Group>
+                        <Form.Group controlId="email">
+                            <Form.Label>Email Address</Form.Label>
+                            <Form.Control required type="email" placeholder="Enter Email" />
+                        </Form.Group>
+                        <Form.Group controlId="password">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control required type="password" placeholder="Password" />
+                        </Form.Group>
+                        <div className={styles.flex_reverse}>
+                            <Button variant="primary" type="submit">
+                                Sign Up
+                            </Button>
+                        </div>
+                    </Form>
+                </Col>
+                <Col sm={12} md={5} lg={8} className={styles.center_align_items}>
+                    <div>
+                        Sign up today and be awesome.
                     </div>
                 </Col>
-                <Col xs={0} md={2} lg={3}></Col>
             </Row>
         </Container>
     )
