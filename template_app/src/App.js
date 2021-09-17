6855adb7a076b60dd8e3550ca8732b8ee5d4fab6
Matt Chapman <eatsalldaylol@hotmail.com>
2021-09-16 20:48:00 -0400

    Login Page

--- template_app/src/App.js
+++ template_app/src/App.js
@@ -1,14 +1,15 @@
 import './styles/App.scss'
-import { SignupPage } from './pages/public/signupPage'
+import { LoginPage } from './pages/public/loginPage'
 import { NavigationTop } from './components/navagation/navTop'
 import { Footer } from './components/navagation/footer'
 import 'bootstrap/dist/css/bootstrap.min.css'
+
 
 function App() {
   return (
     <div className="App">
       <NavigationTop />
-      <SignupPage />
+      <LoginPage />
       <Footer />
     </div>
   )
--- template_app/src/pages/public/loginPage.js
+++ template_app/src/pages/public/loginPage.js
@@ -1,10 +1,39 @@
 import React from 'react'
 import styles from './loginPage.module.scss'
+import { Container, Row, Col, Form, Button } from 'react-bootstrap'
 
 export const LoginPage = () => {
+    const handleSignUp = async (e) => {
+        e.preventDefault()
+    }
+
     return (
-        <div className={styles.login_background}>
-            
-        </div>
+        <Container fluid className={styles.signup_background}>
+            <Row className={styles.row_padding}>
+                <Col xs={0} sm={0} md={1} lg={1}></Col>
+                <Col sm={12} md={6} lg={3} className={styles.row_padding}>
+                    <Form onSubmit={handleSignUp}>
+                        <Form.Group controlId="email">
+                            <Form.Label>Email Address</Form.Label>
+                            <Form.Control required type="email" placeholder="Enter Email" />
+                        </Form.Group>
+                        <Form.Group controlId="password">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control required type="password" placeholder="Password" />
+                        </Form.Group>
+                        <div className={styles.flex_reverse}>
+                            <Button variant="primary" type="submit">
+                                Login
+                            </Button>
+                        </div>
+                    </Form>
+                </Col>
+                <Col sm={12} md={5} lg={8} className={styles.center_align_items}>
+                    <div>
+                        You are awesome.
+                    </div>
+                </Col>
+            </Row>
+        </Container>
     )
 }
