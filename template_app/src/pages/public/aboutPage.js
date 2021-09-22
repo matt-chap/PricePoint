a0432d41894745dfcf9abfd131c28177755251e9
Matt Chapman <eatsalldaylol@hotmail.com>
2021-09-21 22:39:42 -0400

    Add About Page sty

--- template_app/src/pages/public/aboutPage.js
+++ template_app/src/pages/public/aboutPage.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import styles from './aboutPage.module.scss'
+import { Container, Row, Col } from 'react-bootstrap'
 
 export const AboutPage = () => {
     return (
-        <div className={styles.about_background}>
-            
-        </div>
+        <Container fluid className={styles.contactUs_background}>
+            <Row className={styles.row_padding}>
+                <Col sm={0} md={2} lg={2} className={styles.center_align_items}></Col>
+                <Col sm={12} md={8} lg={8} className={styles.center_align_items}></Col>
+                <Col sm={0} md={2} lg={2} className={styles.center_align_items}></Col>
+            </Row>
+        </Container>
     )
 }
--- template_app/src/pages/public/contactUsPage.js
+++ template_app/src/pages/public/contactUsPage.js
@@ -7,6 +7,8 @@ export const ContactUsPage = () => {
         <Container fluid className={styles.contactUs_background}>
             <Row className={styles.row_padding}>
                 <Col sm={0} md={2} lg={2} className={styles.center_align_items}></Col>
+                <Col sm={12} md={8} lg={8} className={styles.center_align_items}></Col>
+                <Col sm={0} md={2} lg={2} className={styles.center_align_items}></Col>
             </Row>
         </Container>
     )
