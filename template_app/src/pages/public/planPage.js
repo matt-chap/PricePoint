cc4344fc1625bd213786a7d42f89433e30f1ec6e
Matt Chapman <eatsalldaylol@hotmail.com>
2021-09-22 19:18:40 -0400

    plan page

--- template_app/src/pages/public/planPage.js
+++ template_app/src/pages/public/planPage.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import styles from './planPage.module.scss'
+import { Container, Row, Col } from 'react-bootstrap'
 
 export const PlanPage = () => {
     return (
-        <div className={styles.plan_background}>
-
-        </div>
+        <Container fluid className={styles.signup_background}>
+            <Row className={styles.row_padding}>
+                <Col xs={0} sm={0} md={1} lg={1}></Col>
+                <Col sm={12} md={6} lg={3} className={styles.row_padding}></Col>
+                <Col sm={12} md={5} lg={8} className={styles.center_align_items}></Col>
+            </Row>
+        </Container>
     )
 }
