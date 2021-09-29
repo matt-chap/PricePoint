50aa061233249f48e69962f7967c4808f8deff50
Matt Chapman <eatsalldaylol@hotmail.com>
2021-09-29 17:58:17 -0400

    Css page changes

--- template_app/src/pages/account/accountManagementPage.js
+++ template_app/src/pages/account/accountManagementPage.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import styles from './accountManagementPage.module.scss'
+import { Container, Row, Col } from 'react-bootstrap'
 
 export const AccountManagementPage = () => {
     return (
-        <div>
-            
-        </div>
+        <Container fluid className={styles.account_background}>
+            <Row className={styles.row_padding}>
+                <Col sm={0} md={2} lg={2} className={styles.center_align_items}></Col>
+                <Col sm={12} md={8} lg={8} className={styles.center_align_items}></Col>
+                <Col sm={0} md={2} lg={2} className={styles.center_align_items}></Col>
+            </Row>
+        </Container>
     )
 }
