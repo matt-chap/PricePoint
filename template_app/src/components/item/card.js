8587ecba4235f7f46d258b8095ca101b72d5c1fa
Matt Chapman <eatsalldaylol@hotmail.com>
2021-09-30 22:08:09 -0400

    CSS updates

--- template_app/src/components/item/card.js
+++ template_app/src/components/item/card.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Typography, CardMedia, CardContent, CardActionArea, Grid }from '@material-ui/core'
+import { Typography, CardContent, CardActionArea, Grid } from '@material-ui/core'
 import { Col } from 'react-bootstrap'
 
 export const CardItem = ({ item }) => {
@@ -7,34 +7,16 @@ export const CardItem = ({ item }) => {
     // Still neeed to style
     return (
         <Col lg={4} md={6} xl={3} >
-            <CardActionArea style={{height: '100%'}}>
-                {modifiedLocation
-                    ? <CardMedia
-                        image={item.ImageLocation}
-                        title={item.Name}
-                        style={{opacity: !modifiedLocation ? '0.25' : null}}
-                    />
-                    : <div 
-                        style={{
-                            minHeight: '140px', backgroundColor: 'white', 
-                            padding: '20px', textAlign: 'center',
-                            borderRadius: '10px', marginTop: '5px',
-                            display: 'flex', alignItems: 'center',
-                            justifyContent: 'center'
-                        }}
-                        >
-                        <h2>{item.Name}</h2>
-                    </div>
-                }
+            <CardActionArea style={{ height: '100%' }}>
                 <CardContent>
                     <Grid container spacing={2}>
                         <Grid item xs={12}>
-                            <Typography variant="h5" style={{fontSize: '1.5rem'}}>
+                            <Typography variant="h5" style={{ fontSize: '1.5rem' }}>
                                 {item.Name}
                             </Typography>
                         </Grid>
                         <Grid item xs={12}>
-                            <Typography variant="h5" noWrap style={{fontSize: '.75rem'}}>
+                            <Typography variant="h5" noWrap style={{ fontSize: '.75rem' }}>
                                 {item.Description}
                             </Typography>
                         </Grid>
