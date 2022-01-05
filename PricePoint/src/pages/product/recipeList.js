012cf24bdac42ea6a5f8d5aa3282250776938757
matt-chap <eatsalldaylol@hotmail.com>
2022-01-04 19:47:08 -0500

    Recipe Fix ingredient Name

--- PricePoint/src/pages/product/recipeList.js
+++ PricePoint/src/pages/product/recipeList.js
@@ -1,7 +1,24 @@
 import React from 'react'
 import styles from './recipeList.module.scss'
 import { Table, Row, Col } from 'react-bootstrap'
-import { fakeRecipes, AmountType } from '../../data/fakeData'
+import { fakeRecipes, AmountType, fakeIngredients } from '../../data/fakeData'
+
+let fakeRecipiesTransformed = fakeRecipes.map(x => {
+
+
+    return ({
+        Recipe: x.Recipe,
+        Ingrdients: x.Ingrdients.map((v) => {
+            let ing = fakeIngredients.filter(z => z.IngredientId == v.IngredientId)[0]
+
+            return ({
+                IngredientName: ing?.Ingredient ?? "Unknown",
+                AmountType: v.AmountType,
+                Amount: v.Amount
+            })
+        })
+    })
+})
 
 export const RecipeList = () => {
     return (
@@ -13,32 +30,34 @@ export const RecipeList = () => {
                 <Col sm={0} md={2} lg={2} className={styles.center_align_items}></Col>
             </Row>
             <Row className={styles.row_padding}>
-                    <Col xs sm lg className={styles.center_align_items}>
-                        <Table striped bordered hover>
-                            <thead>
-                                <tr>
-                                    <th className={styles.recipe_data}>Recipe</th>
-                                    <th className={styles.recipe_data}>Ingrdients</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {fakeRecipes && fakeRecipes.map((x) => {
-                                    return (
-                                        <tr key={x.RecipeId}>
-                                            <td className={styles.recipe_data}>{x.Recipe}</td>
-                                            <td className={styles.recipe_data}>{x.Ingrdients && x.Ingrdients.map((v) => { return (
-                                                "IngredientId " + v.IngredientId + "\n" +
-                                                "Amount Type " + Object.keys(AmountType)[v.AmountType ] + "\n" +
-                                                "Amount " + v.Amount + "\n" 
-                                            )})}</td>
-                                        </tr>
-                                    )
-                                }
-                                )}
-                            </tbody>
-                        </Table>
-                    </Col>
-                </Row>
+                <Col xs sm lg className={styles.center_align_items}>
+                    <Table striped bordered hover>
+                        <thead>
+                            <tr>
+                                <th className={styles.recipe_data}>Recipe</th>
+                                <th className={styles.recipe_data}>Ingrdients</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {fakeRecipiesTransformed && fakeRecipiesTransformed.map((x) => {
+                                return (
+                                    <tr key={x.RecipeId}>
+                                        <td className={styles.recipe_data}>{x.Recipe}</td>
+                                        <td className={styles.recipe_data}>{x.Ingrdients && x.Ingrdients.map((v) => {
+                                            return (
+                                                "Ingredient Name " + v.IngredientName + "\n" +
+                                                "Amount Type " + Object.keys(AmountType)[v.AmountType] + "\n" +
+                                                "Amount " + v.Amount + "\n"
+                                            )
+                                        })}</td>
+                                    </tr>
+                                )
+                            }
+                            )}
+                        </tbody>
+                    </Table>
+                </Col>
+            </Row>
         </Col>
     )
 }
