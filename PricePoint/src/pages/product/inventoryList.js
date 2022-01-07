bbea936f47fef3d03201a15eb3d63c08e3aa17f2
matt-chap <eatsalldaylol@hotmail.com>
2022-01-06 22:30:27 -0500

    Adding inventory

--- PricePoint/src/pages/product/inventoryList.js
+++ PricePoint/src/pages/product/inventoryList.js
@@ -20,8 +20,8 @@ export const InventoryList = () => {
         <Col sm={11} md={11} lg={11} className={styles.inventory_background}>
             <h1>Inventory List</h1>
             <Row className={styles.row_padding}>
-                <Col sm={0} md={2} lg={2} className={styles.center_align_items}></Col>
-                <Col sm={12} md={8} lg={8} className={styles.center_align_items}></Col>
+                <Col sm={0} md={2} lg={2} className={styles.center_align_items}>Add</Col>
+                <Col sm={12} md={8} lg={8} className={styles.center_align_items}><input type='text'></input></Col>
                 <Col sm={0} md={2} lg={2} className={styles.center_align_items}></Col>
             </Row>
             <Row className={styles.row_padding}>
